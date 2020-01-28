8c37d56407149b360799d1ba287ce4fc6713f486
jinshi <js_smith@163.com>
2020-01-28 18:37:14 +0800

    add chapter03 demo04 checkbox

---
+++ flutterNotebook/lib/chapter-03/demo04.dart
@@ -0,0 +1,60 @@
+///
+/// Flutter 中的单选框和复选框
+///
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('单选矿和复选框'),),
+        body: SwitchAndCheckBoxTestRoute(),
+      ),
+    );
+  }
+}
+
+class SwitchAndCheckBoxTestRoute extends StatefulWidget {
+  SwitchAndCheckBoxTestRoute({Key key}) : super(key: key);
+
+  @override
+  _SwitchAndCheckBoxTestRouteState createState() =>
+      _SwitchAndCheckBoxTestRouteState();
+}
+
+class _SwitchAndCheckBoxTestRouteState
+    extends State<SwitchAndCheckBoxTestRoute> {
+  bool _switchSelected = true; //维护单选开关状态
+  bool _checkboxSelected = true; //维护复选框状态
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Switch(
+          value: _switchSelected, //当前状态
+          onChanged: (value) {
+            //重新构建页面
+            setState(() {
+              _switchSelected = value;
+            });
+          },
+        ),
+        Checkbox(
+          value: _checkboxSelected,
+          activeColor: Colors.red, //选中时的颜色
+          onChanged: (value) {
+            setState(() {
+              _checkboxSelected = value;
+            });
+          },
+        )
+      ],
+    );
+  }
+}
