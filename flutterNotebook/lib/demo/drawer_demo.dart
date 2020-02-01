5106f9bc3ccd3e6f933eb0c7616eba2192f83551
jinshi <js_smith@163.com>
2020-02-01 21:55:18 +0800

    add project

---
+++ flutterNotebook/lib/demo/drawer_demo.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class DrawerDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          UserAccountsDrawerHeader(
+            accountName:
+                Text('jsfantasy', style: TextStyle(fontWeight: FontWeight.bold)),
+            accountEmail: Text('jssmith0716@gmail.com'),
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://s2.ax1x.com/2020/02/01/1GXm5T.png'),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.yellow[400],
+              image: DecorationImage(
+                image: NetworkImage(
+                    'https://resources.ninghao.org/images/childhood-in-a-picture.jpg'),
+                fit: BoxFit.cover,
+                colorFilter: ColorFilter.mode(
+                    Colors.yellow[400].withOpacity(0.6), BlendMode.hardLight),
+              ),
+            ),
+          ),
+          ListTile(
+            title: Text(
+              'Messages',
+              textAlign: TextAlign.right,
+            ),
+            trailing: Icon(Icons.message, color: Colors.black12, size: 22.0),
+            onTap: () => Navigator.pop(context),
+          ),
+          ListTile(
+            title: Text(
+              'Favorite',
+              textAlign: TextAlign.right,
+            ),
+            trailing: Icon(Icons.favorite, color: Colors.black12, size: 22.0),
+            onTap: () => Navigator.pop(context),
+          ),
+          ListTile(
+            title: Text(
+              'Settings',
+              textAlign: TextAlign.right,
+            ),
+            trailing: Icon(Icons.settings, color: Colors.black12, size: 22.0),
+            onTap: () => Navigator.pop(context),
+          ),
+        ],
+      ),
+    );
+  }
+}
