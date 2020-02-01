6c979f9da6dec7c1d6211c692535365e072f3f14
jinshi <js_smith@163.com>
2020-02-01 21:05:02 +0800

    update

--- flutterNotebook/lib/chapter-02/demo-02.dart
+++ flutterNotebook/lib/chapter-02/demo-02.dart
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             new Text(
               '$_counter',
-              style: Theme.of(context).textTheme.display1,
+              style: Theme.of(context).textTheme.headline4,
             ),
             new FlatButton(
               child: new Text('Open new route page'),
--- flutterNotebook/lib/chapter-02/demo-03.dart
+++ flutterNotebook/lib/chapter-02/demo-03.dart
@@ -104,7 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             new Text(
               '$_counter',
-              style: Theme.of(context).textTheme.display1,
+              style: Theme.of(context).textTheme.headline4,
             ),
             new FlatButton(
               child: Text("命名路由实例"),
