e0fb07fc8a917003ad87c61f44f9f77cffbcd88a
jinshi <js_smith@163.com>
2020-02-04 09:24:15 +0800

    add dio example

--- flutter_shop/lib/pages/home_page.dart
+++ flutter_shop/lib/pages/home_page.dart
@@ -1,14 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController typeController = TextEditingController();
+  String showText = '欢迎你来到美好人间';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('商城首页')
-      ),
+    return Container(
+      child: Scaffold(
+          appBar: AppBar(title: Text('图书类型')),
+          body: Container(
+            height: 1000,
+            child: Column(
+              children: <Widget>[
+                TextField(
+                  controller: typeController,
+                  decoration: InputDecoration(
+                      contentPadding: EdgeInsets.all(10.0),
+                      labelText: '美女类型',
+                      helperText: '请输入你喜欢的类型'),
+                  autofocus: false,
+                ),
+                RaisedButton(
+                  onPressed: _choiceAction,
+                  child: Text('选择完毕'),
+                ),
+                Text(
+                  showText,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                ),
+              ],
+            ),
+          )),
     );
+  }
+
+  void _choiceAction() {
+    if (typeController.text.toString() == '') {
+      showDialog(
+          context: context,
+          builder: (context) => AlertDialog(title: Text('美女类型不能为空')));
+    } else {
+      getHttp(typeController.text.toString()).then((val) {
+        setState(() {
+          showText = val['data'][0]['LongGoodsName'].toString();
+        });
+      });
+    }
+  }
+
+  Future getHttp(String id) async {
+    var path =
+        'http://api.xuandan.com/DataApi/item';
+    var data = {
+      'appkey': '5wfidtll6t',
+      'id': id,
+    };
+    try {
+      Response response;
+      response = await Dio().get(path, queryParameters: data);
+      return response.data;
+    } catch (e) {
+      return print(e);
+    }
   }
 }
--- flutter_shop/lib/pages/index_page.dart
+++ flutter_shop/lib/pages/index_page.dart
@@ -52,9 +52,6 @@ class _IndexPageState extends State<IndexPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('百姓生活'),
-      ),
       backgroundColor: Color.fromRGBO(244, 245, 245, 1.0),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
